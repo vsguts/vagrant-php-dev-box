aba892b0efffd034dc21cab347578c1e0a4c2697
gvs <gvs@simtech.ru>
2013-12-03 17:46:37 +0400

    Tweak fixes

--- recipes/bootstrap.sh
+++ recipes/bootstrap.sh
@@ -19,25 +19,27 @@ cp /etc/apache2/sites-available/default /etc/apache2/sites-available/default.bak
 sed -i '9,15s/AllowOverride None/AllowOverride All/g' /etc/apache2/sites-available/default
 
 # php
-apt-get install -q -y php5 php-pear php5-dev php5-mysql php5-curl
+apt-get install -q -y php5 php-pear php5-dev php5-mysql php5-curl make
 cp /etc/php5/apache2/php.ini /etc/php5/apache2/php.ini.bak
 sed -i 's/display_errors = Off/display_errors = On/g' /etc/php5/apache2/php.ini
 sed -i 's/html_errors = Off/html_errors = On/g' /etc/php5/apache2/php.ini
 sed -i 's/post_max_size = 8M/post_max_size = 128M/g' /etc/php5/apache2/php.ini
 sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 122M/g' /etc/php5/apache2/php.ini
+sed -i 's/session.gc_maxlifetime = 1440/session.gc_maxlifetime = 86400/g' /etc/php5/apache2/php.ini
 pecl install xdebug
 echo "" >> /etc/php5/apache2/php.ini
 echo "# gvs" >> /etc/php5/apache2/php.ini
 echo "extension=xdebug.so" >> /etc/php5/apache2/php.ini
+pecl install redis
 
 # additions
 apt-get install -q -y redis-server
-# TODO: php-redis
 
 # final
 service apache2 restart
 
 # common
+apt-get install -q -y postfix
 apt-get install -q -y zsh
 apt-get install -q -y git
 apt-get install -q -y curl
@@ -48,4 +50,7 @@ apt-get install -q -y tmux screen
 
 
 # tools
-# apt-get install -q -y phpmyadmin
+apt-get install -q -y phpmyadmin
+
+# post:
+# dpkg-reconfigure phpmyadmin
